5bb38b6130c28430a970756361fe5cead9e21e7e
Safi <mdsafi729@gmail.com>
2023-05-06 14:13:42 +0530

    [shamsuddin]Fix: Adding UC2 file

---
+++ UC2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter your last name: ");
+        string lastName = Console.ReadLine();
+
+        string pattern = @"^[A-Z][a-z]{2,}$";
+
+        if (Regex.IsMatch(lastName, pattern))
+        {
+            Console.WriteLine("Valid last name entered: " + lastName);
+        }
+        else
+        {
+            Console.WriteLine("Invalid last name entered. Last name should start with a capital letter and have at least 3 characters.");
+        }
+    }
+}
