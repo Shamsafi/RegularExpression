0d27336b6dec1826b8db95813e1ba1b24ab1ded5
Safi <mdsafi729@gmail.com>
2023-05-06 14:18:15 +0530

    [shamsuddin]Fix: Adding UC7 file

---
+++ UC7.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your password:");
+            string password = Console.ReadLine();
+
+            try
+            {
+                //Must contain at least one numeric number
+                Regex regex = new Regex(@"^(?=.*\d)(?!.*[^\w\s])(?!.*[_]).{8,}$");
+
+                if (regex.IsMatch(password))
+                {
+                    Console.WriteLine("Success: Valid password.");
+                }
+                else
+                {
+                    throw new Exception("Error: Invalid password.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
