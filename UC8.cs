c803298d3addc3f77a11bb716b174a7ce9b82be0
Safi <mdsafi729@gmail.com>
2023-05-06 14:19:17 +0530

    [shamsuddin]Fix: Adding UC8 file

---
+++ UC8.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {   // Must have 1 special char
+        try
+        {
+            Console.Write("Enter your password: ");
+            string password = Console.ReadLine();
+
+            if (!IsValidPassword(password))
+            {
+                throw new Exception("Invalid password entered.");
+            }
+
+            Console.WriteLine("Valid password entered: " + password);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    private static bool IsValidPassword(string password)
+    {
+        string regexPattern = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@#$%^&+=!])[^\\s]{8,}$";
+
+        bool isMatch = Regex.IsMatch(password, regexPattern);
+
+        return isMatch;
+    }
+}
