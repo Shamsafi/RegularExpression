d9794a13c2703e7c063f9aa57244309cfc392305
Safi <mdsafi729@gmail.com>
2023-05-06 14:13:02 +0530

    [shamsuddin]Fix: Adding UC1 file

---
+++ UC1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class UC1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first name:");
+            string firstName = Console.ReadLine();
+
+            if (Regex.IsMatch(firstName, @"^[A-Z][a-z]{2,}$"))
+            {
+                Console.WriteLine("Success: Valid first name.");
+            }
+            else
+            {
+                Console.WriteLine("Error: Invalid first name.");
+            }
+        }
+    }
+}
