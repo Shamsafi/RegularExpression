a7bbec958cd52d509f46c2503194d6e4702a9bf5
Safi <mdsafi729@gmail.com>
2023-05-06 14:17:03 +0530

    [shamsuddin]Fix: Adding UC6 file

---
+++ UC6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your password:");
+            string password = Console.ReadLine();
+
+            try
+            {
+
+                //Should have at least 1 Upper Case - NOTE – All rules must
+                Regex regex = new Regex(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?!.*[^\w\s]).{8,}$");
+
+                if (regex.IsMatch(password))
+                {
+                    Console.WriteLine("Success: Valid password.");
+                }
+                else
+                {
+                    throw new Exception("Error: Invalid password.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
