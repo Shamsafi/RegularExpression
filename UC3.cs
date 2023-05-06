dba70101921dfb2910f605610b2e7c5a3d0fca54
Safi <mdsafi729@gmail.com>
2023-05-06 14:14:41 +0530

    [shamsuddin]Fix: Adding UC3 file

---
+++ UC3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your email:");
+            string email = Console.ReadLine();
+
+            Regex regex = new Regex(@"^[a-z0-9]+([._+\-][a-z0-9]+)*@[a-z0-9]+(\.[a-z]{2,3}){1,2}$");
+
+            if (regex.IsMatch(email))
+            {
+                Console.WriteLine("Success: Valid email address.");
+            }
+            else
+            {
+                Console.WriteLine("Error: Invalid email address.");
+            }
+        }
+    }
+}
