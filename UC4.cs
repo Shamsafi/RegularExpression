a00f613afea307ea35d915901f4fa6f7a35cbea9
Safi <mdsafi729@gmail.com>
2023-05-06 14:15:41 +0530

    [shamsuddin]Fix: Adding UC4 file

---
+++ UC4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your mobile number:");
+            string mobileNumber = Console.ReadLine();
+
+            Regex regex = new Regex(@"^91\s[1-9][0-9]{9}$");
+
+            if (regex.IsMatch(mobileNumber))
+            {
+                Console.WriteLine("Success: Valid mobile number.");
+            }
+            else
+            {
+                Console.WriteLine("Error: Invalid mobile number.");
+            }
+        }
+    }
+}
